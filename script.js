6f6081a44af3e4f8a51284979885fb17a93cfb3b
gopalptl14 <gopalptl2350@gmail.com>
2023-05-27 23:47:39 +0530

    solve question

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const colorSelect = document.getElementById("colorSelect");
+// console.log(colorSelect);
+
+function removeColor(){
+    const optionValue = colorSelect.value;
+    // console.log(optionValue);
+    const options = document.getElementsByTagName("option");
+    // console.log(options[2].value);
+   
+    for(const deleteoption of options){
+        if(deleteoption.value === optionValue){
+            // console.log(option.value);
+            deleteoption.remove();
+        }
+    }
+
+}
